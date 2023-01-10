8d86f83bffcafdeee2b8fe62ef7297c875943d81
dariussingh <darius2121.ds@gmail.com>
2023-01-10 19:16:48 +0530

    get_data.py, load_data.py, split_data.py files

--- src/get_data.py
+++ src/get_data.py
@@ -6,16 +6,14 @@ from pkgutil import get_data
 
 def get_data(config_path):
     config = read_params(config_path)
-    print(config)
+    data_path = config['data_source']["s3_source"]
+    df = pd.read_csv(data_path, sep=",", encoding="utf-8")
+    return df
 
 def read_params(config_path):
     with open(config_path) as yaml_file:
         config = yaml.safe_load(yaml_file)
         return config
-
-
-
-
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
